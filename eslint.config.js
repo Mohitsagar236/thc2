977cea4c8fc1e275ce4f46cfebea293761331f9e
Mohitsagar236 <cp8137108@gmail.com>
2026-04-10 19:40:29 +0530

    use of talwind

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ import prettier from "eslint-plugin-prettier";
 import prettierConfig from "eslint-config-prettier";
 
 export default tseslint.config(
-  { ignores: ["dist", "node_modules", "build", ".vite", "coverage"] },
+  { ignores: ["dist"] },
   {
     extends: [
       js.configs.recommended,
